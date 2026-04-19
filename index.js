21713f02e379c9d8e96ee9769190d7996ee465fb
xnak3 <59103074+Yagami1018@users.noreply.github.com>
2026-04-19 12:19:54 -0400

    Add event listener for add button

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ function Book(author, title, pages, isRead) {
     const id = crypto.randomUUID();
     this.author = author;
     this.title = title;
-    this.pages = pages;
+    this.pages = pages || 0;
     this.isRead = isRead;
 }
 
-function addBookToLibrary(Book){
+function addBookToLibrary(Book) {
     library.push(Book);
 }
+
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const pages = document.getElementById('pages');
+const reading = document.getElementById('reading');
+const addBtn = document.querySelector('.add');
+const resetBtn = document.querySelector('.reset');
+
+addBtn.addEventListener('click', e => {
+    e.preventDefault();
+    const newBook = new Book(author.value, title.value, pages.value, reading.checked);
+    console.log(newBook);
+});
